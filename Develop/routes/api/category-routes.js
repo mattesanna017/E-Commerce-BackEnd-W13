5616aa50e1f5357706329f35b25ca11bd642f557
mattesanna017 <matteo.mog.sanna@gmail.com>
2022-07-14 17:17:13 +0100

    category seeds

--- Develop/routes/api/category-routes.js
+++ Develop/routes/api/category-routes.js
@@ -3,10 +3,20 @@ const { Category, Product } = require('../../models');
 
 // The `/api/categories` endpoint
 
-router.get('/', (req, res) => {
-  // find all categories
-  // be sure to include its associated Products
+// find all categories
+// be sure to include its associated Products
+router.get('/', async (req, res) => {
+  try {
+    const CategoryData = await Category.findAll({
+      include: [{ model: Product }],
+    });
+    res.status(200).json(CategoryData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
+
+
 
 router.get('/:id', (req, res) => {
   // find one category by its `id` value
